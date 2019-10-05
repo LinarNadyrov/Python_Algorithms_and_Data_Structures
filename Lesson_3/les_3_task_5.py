c855991c3e69c891e60b0f2d1e673d712a6c7a32
Linar <linar.nadyrov@itbegin.ru>
2019-10-05 22:43:32 +0300

    Lesson-3

---
+++ Lesson_3/les_3_task_5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+# В массиве найти максимальный отрицательный элемент. 
+# Вывести на экран его значение и позицию в массиве.
+
+from random import random
+N = 5
+arr = []
+for i in range(N):
+        arr.append(int(random() * 20) - 10)
+print(arr)
+ 
+i = 0
+index = -1
+while i < N:
+        if arr[i] < 0 and index == -1:
+                index = i
+        elif arr[i] < 0 and arr[i] > arr[index]:
+                index = i
+        i += 1
+ 
+print(' Максимальный отрицательный элемент: ', arr[index], '\n','Позиция: ', index+1 )
