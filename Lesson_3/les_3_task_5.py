0f050760b0c2369af2cc1458a81295464d2ce4e5
Linar <linar.nadyrov@itbegin.ru>
2019-10-08 07:35:34 +0300

    Lesson-3

--- Lesson_3/les_3_task_5.py
+++ Lesson_3/les_3_task_5.py
@@ -16,7 +16,7 @@ index = -1
 while i < N:
         if arr[i] < 0 and index == -1:
                 index = i
-        elif arr[i] < 0 and arr[i] > arr[index]:
+        elif arr[i] < 0 and arr[i] < arr[index]:
                 index = i
         i += 1
  
