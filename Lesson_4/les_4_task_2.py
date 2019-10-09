5609a04b2f2d160980b76151b1e49c04b6112863
Linar <linar.nadyrov@itbegin.ru>
2019-10-09 23:32:46 +0300

    Lesson-4

--- Lesson_4/les_4_task_2.py
+++ Lesson_4/les_4_task_2.py
@@ -19,71 +19,62 @@ import cProfile
 import random
 from random import random
 
+count = int(input('Введите размер массива: '))
+array = [0] * count
+for i in range(count):
+    array[i] = int(random() * 100)
+print(array)
+
 ## ВАРИАНТ №1
-
-N = 5
-arr = [0]*N
-print('Изначальный массив: ')
-for i in range(N):
-    arr[i] = int(random()*5)
-    print( arr[i], end = ' ')
-print()
-
-# mn = 0
-# mx = 0
-# for i in range(N):
-#     if arr[i] < arr[mn]:
-#         mn = i
-#     elif arr[i] > arr[mx]:
-#         mx = i
-# b = arr[mn]
-# arr[mn] = arr[mx]
-# arr[mx] = b
-
-# ## ВАРИАНТ №2
-mn = min(arr)
-mx = max(arr)
-imn = arr.index(mn)
-imx = arr.index(mx)
-print('arr[%d]=%d arr[%d]=%d' % (imn+1, mn, imx+1, mx))
-arr[imn],arr[imx] = arr[imx],arr[imn]
+mn = 0
+mx = 0
+for i in range(count):
+    if array[i] < array[mn]:
+        mn = i
+    elif array[i] > array[mx]:
+        mx = i
+b = array[mn]
+array[mn] = array[mx]
+array[mx] = b
 
 print('Поменяли местали max и min элементы: ') 
-for i in range(N):
-    print(arr[i], end=' ')
+for i in range(count):
+    print(array[i], end = ' ')
+print()
+
+## ВАРИАНТ №2
+mn = min(array)
+mx = max(array)
+imn = array.index(mn)
+imx = array.index(mx)
+array[imn],array[imx] = array[imx],array[imn]
+
+print('Поменяли местали max и min элементы: ') 
+for i in range(count):
+    print(array[i], end = ' ')
 print()
 
 ## ВАРИАНТ №3
-# count = int(input('Введите размер массива: '))
-# a = [0] * count
-# for i in range(count):
-#     a[i] = int(random() * 100)
-# print(a)
-
-# min = 0
-# max = 0
-# for i in range(count):
-#     if a[i] < a[min]:
-#         min = i
-#     elif a[i] > a[max]:
-#         max = i
-# b = a[min]
-# a[min] = a[max]
-# a[max] = b
-# print(a)
+array[array.index(max(array))], array[array.index(min(array))] = array[array.index(min(array))], array[array.index(max(array))]
+print(array)
 
 # Тест с помощью timeit 
-# Тест на размере массиа = 4 
-#  - ВАРИАНТ №1
-#  - ВАРИАНТ №2
+# Массив из 3 элементов
+# 100 loops, best of 3: 0.0191 usec per loop - ВАРИАНТ №1
+# 100 loops, best of 3: 0.031 usec per loop - ВАРИАНТ №2
+# 100 loops, best of 3: 0.0405 usec per loop - ВАРИАНТ №3
+
+# Массив из 9 элементов
+# 100 loops, best of 3: 0.0191 usec per loop - ВАРИАНТ №1
+# 100 loops, best of 3: 0.031 usec per loop - ВАРИАНТ №2
+# 100 loops, best of 3: 0.0501 usec per loop - ВАРИАНТ №3
+
+# Массив из 18 элементов
+# 100 loops, best of 3: 0.0191 usec per loop - ВАРИАНТ №1
+# 100 loops, best of 3: 0.0286 usec per loop - ВАРИАНТ №2
 # 100 loops, best of 3: 0.0191 usec per loop - ВАРИАНТ №3
 
-# Тест на размере массиа = 14 
-#  - ВАРИАНТ №1
-#  - ВАРИАНТ №2
-# 100 loops, best of 3: 0.0286 usec per loop - ВАРИАНТ №3
-
-# Тест на размере массиа = 114
-#  - ВАРИАНТ №1
-#  - ВАРИАНТ №2
-# 100 loops, best of 3: 0.0191 usec per loop - ВАРИАНТ №3
+# Анализ с помощью модуля timeit показал, что 
+# для массива не больше 3 элементов оптимально использовать ВАРИАНТ №2
+# для массива больше 9 элементов оптимально использовать ВАРИАНТ №2
+# для массива больше 10 элементов результаты у всех трех ВАРИАНТОВ прибризительно равны
--- Lesson_4/les_4_task_3.py
+++ Lesson_4/les_4_task_3.py
@@ -15,43 +15,75 @@
 # Они могут быть как равны между собой (оба являться минимальными), так и различаться.
 import timeit
 import cProfile
+from random import random
 
-array = [13, 12, 577, 124, 0, 1235, 98, -1, 1, 3, 999, -99]
-# [-29, 21, -43, 7, 15, 34]
-# [-29, 15, 99]
+count = int(input('Введите размер массива: '))
+array = [0] * count
+for i in range(count):
+    array[i] = int(random() * 100)
+print(array)
 
-
+# def array_size(count):
+#     count = int(input('Введите размер массива: '))
+#     array = [0] * count
+#     for i in range(count):
+#         array[i] = int(random() * 100)
+#     return array
 
 ## ВАРИАНТ №1
-# pos1 = 0
-# pos2 = 0
-# for i, item in enumerate(array):
-#     if item <= array[pos1]:
-#         pos2 = pos1
-#         pos1 = i
-#     elif item <= array[pos2]:
-#         pos2 = i
-# print('Два наименьших числа:', array[pos1], ' и ', array[pos2])
+pos1 = 0
+pos2 = 0
+for i, item in enumerate(array):
+    if item <= array[pos1]:
+        pos2 = pos1
+        pos1 = i
+    elif item <= array[pos2]:
+        pos2 = i
+print('Два наименьших числа:', array[pos1], ' и ', array[pos2])
 
 ## ВАРИАНТ №2
-# min1 = min(array)
-# array.remove(min1)
-# min2 = min(array)
-# if min1 == min2:
-#     print('Наименьшее число: ', min1)
-# else: 
-#     print('Два наименьших числа: ', min1, ' и ', min2)
+min1 = min(array)
+array.remove(min1)
+min2 = min(array)
+if min1 == min2:
+    print('Наименьшее число: ', min1)
+else: 
+    print('Два наименьших числа: ', min1, ' и ', min2)
+
+## ВАРИАНТ №3
+if array[0] > array[1]:
+    min1 = 0
+    min2 = 1
+else:
+    min1 = 1
+    min2 = 0
+
+for i in range(2,count):
+    if array[i] < array[min1]:
+        b = min1
+        min1 = i
+        if array[b] < array[min2]:
+            min2 = b
+    elif array[i] < array[min2]:
+        min2 = i
+print('Два наименьших числа: ', array[min1], ' и ', array[min2])
 
 # Тест с помощью timeit 
-# Массив из трех элементов
-# 100 loops, best of 3: 0.0191 usec per loop - ВАРИАНТ №1
+# Массив из 3 элементов
+# 100 loops, best of 3: 0.0405 usec per loop - ВАРИАНТ №1
 # 100 loops, best of 3: 0.0191 usec per loop - ВАРИАНТ №2
-# 100 loops, best of 3: 0.0381 usec per loop - ВАРИАНТ №3
+# 100 loops, best of 3: 0.031 usec per loop - ВАРИАНТ №3
 
-# Массив из шести элементов
-# 100 loops, best of 3: 0.0286 usec per loop - ВАРИАНТ №1
-# 100 loops, best of 3: 0.0215 usec per loop - ВАРИАНТ №2
+# Массив из 9 элементов
+# 100 loops, best of 3: 0.031 usec per loop - ВАРИАНТ №1
+# 100 loops, best of 3: 0.031 usec per loop - ВАРИАНТ №2
+# 100 loops, best of 3: 0.031 usec per loop - ВАРИАНТ №3
 
-# Массив из двенадцати элементов
-# 100 loops, best of 3: 0.00954 usec per loop - ВАРИАНТ №1
+# Массив из 18 элементов
+# 100 loops, best of 3: 0.031 usec per loop - ВАРИАНТ №1
 # 100 loops, best of 3: 0.0286 usec per loop - ВАРИАНТ №2
+# 100 loops, best of 3: 0.0286 usec per loop - ВАРИАНТ №3
+
+# Анализ с помощью модуля timeit показал, что 
+# для массива не больше 3 элементов оптимально использовать ВАРИАНТ №2
+# для массива больше 3 элементов результаты у всех трех ВАРИАНТОВ прибризительно равны
